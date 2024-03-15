230cb0cd1b0eaa2e53da81f97fe3d312a81cf5a1
Francesco Piscani <dev@Francescos-MacBook-Pro.local>
2024-03-14 20:03:52 -0400

    1. Building Initial Page

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Image, Text, View } from "react-native";
+
+import products from "../../../assets/data/products";
+import Colors from "../../constants/Colors";
+const product = products[0];
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontWeight: "600",
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+});
